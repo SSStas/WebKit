5a504b353a2a1ac469b591df18f5d9617e32cfc4
student <student@student-16.local>
2018-04-08 13:09:25 +0300

    add

--- WebKit/ViewController.swift
+++ WebKit/ViewController.swift
@@ -8,18 +8,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    struct Metadata: Codable {
+            let metadata: String
+    }
+
+    
+    var petitions : [[String : String]] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = URL(string: "https://api.whitehouse.gov/v1/petitions.json?limit=1")!
+        
+        if let json = try? String(contentsOf: url) {
+        
+            let data = json.data(using: .utf8)!
+            
+            let decoder = JSONDecoder()
+            let metadata = try! decoder.decode(Metadata.self, from: data)
+            
+            print(metadata)
+        }
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    override func tableView(_ TableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return petitions.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Заголовок"
+        cell.detailTextLabel?.text = "Подзаголовоҝ"
+        return cell
+    }
 
 }
 
